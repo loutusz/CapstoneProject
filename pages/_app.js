4f1f1a73ee93ce071a6b414a0f21e3a431064d4a
jessicaangelita <jessicaangelita1482@gmail.com>
2023-08-13 23:34:33 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import '@/styles/globals.css'
-import { AuthProvider } from '@/Context/AuthContext'
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
       <Component {...pageProps} />
-    </AuthProvider>
   );
 }
