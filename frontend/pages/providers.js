1f3d6d28ae8b1f9712412cef2c5c7047b98d4f4e
jessicaangelita <jessicaangelita1482@gmail.com>
2023-08-17 02:05:47 +0700

    projects & providers page

---
+++ frontend/pages/providers.js
@@ -0,0 +1,10 @@
+import HeaderHome from "@/components/headerhome";
+import React from "react";
+
+export default function Providers() {
+    return (
+        <div>
+            <HeaderHome/>
+        </div>
+    )
+}
