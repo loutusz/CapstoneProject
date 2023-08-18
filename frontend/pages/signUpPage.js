e9b16da752b71e9e93b325c37a2c93ba8d7a9866
Clex1911 <cleospike08@gmail.com>
2023-08-18 19:39:52 +0700

    Update Signup function

--- frontend/pages/signUpPage.js
+++ frontend/pages/signUpPage.js
@@ -1,23 +1,26 @@
 import React, {useState, useRef, useEffect} from 'react';
-// import Link from "next/link";
 import Head from 'next/head';
 import { FaRegEnvelope, FaTimes } from 'react-icons/fa';
 import { MdPermIdentity, MdLockOutline } from 'react-icons/md';
 import { BiSolidHide, BiSolidInfoCircle } from "react-icons/bi";
 import { FcCheckmark} from "react-icons/fc";
+import axios from './api/axios'
 
 // export default function SignUpPage() {
 
 const fullname_valid =  /^[A-Z][a-zA-Z\s]*$/;
-const username_valid = /^[a-zA-Z][a-zA-Z0-9_]{5,15}$/;
-const email_valid = /^[a-zA-Z0-9_]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const username_valid = /^[a-zA-Z][a-zA-Z0-9_]{4,14}$/; //bebas min 5-15 huruf bole spasi
+const email_valid = /^[a-zA-Z0-9_.]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //dapat menggunakan angka, huruf, _ dan . sebelum @
 const pass_valid = /^(?=.*[a-z])(?=.*\d)[a-zA-Z0-9\s]{6,10}$/;      //ada satu huruf dan angka yang required dengan min.6-10 huruf 
+const signup_url = '/signUpPage'
 
 const SignUpPage = () => {
     const userReference = useRef();
     const errReference = useRef();  
+   
     // const fullnameReference = useRef();
     // const emailReference = useRef();  
+    // const [modal, setshowModal] = useState(false)
 
     const [fullname, setFullname] = useState('');
     const [validFullname, setValidFullname] = useState(false);
@@ -74,20 +77,42 @@ const SignUpPage = () => {
     useEffect (() => {
       setErrMsg('');
     }, [fullname, uname, email, pass])
-
-    
-      const handleSubmit = (e) => {
+  
+      const handleSubmit = async  (e) => {
         e.preventDefault();
+        
+        try {
+          const resp = await axios.post(signup_url,
+            JSON.stringify({fullname, uname, email, pass}),{
+              headers: {'Content-Type': 'application/json'},
+              withCredentials: true
+            }
+          )
+          console.log(resp.data);
+          // console.log(resp.accesstoken)
+          console.log(JSON.stringify(resp));
+          setSuccess(true);
 
-        console.log('Pendaftaran berhasil');
-        console.log(formData);
+        } catch(err){
+          if (!err.resp){
+            setErrMsg('No Server Response')
+          }else{
+            setErrMsg('Registration Failed')
+          }
+          errReference.current.focus();
+        }
+
+        // console.log(fullname, uname, email, pass)
+        // console.log('Pendaftaran berhasil');
+        // setSuccess(true);
+        // console.log(formData);
         // Reset form setelah pengiriman
-        setFormData({
-          name: '',
-          username: '',
-          email: '',
-          password: ''
-        });
+        // setFormData({
+        //   name: '',
+        //   username: '',
+        //   email: '',
+        //   password: ''
+        // });
       };
     
             // const useForm = () => {
@@ -101,31 +126,6 @@ const SignUpPage = () => {
       // const handleChange = (e) => {
       //   setFormData({ ...formData, [e.target.name]: e.target.value });
       // };
-
-        // Lakukan aksi pendaftaran atau validasi form di sini
-        // const fullname = /^[A-Z][a-zA-Z\s]+$/;
-        // if(!fullname.test(formData.fullname)) {
-        //   alert('Nama harus diawali dengan huruf kapital');
-        //   return;
-        // }
-
-        // const username = /^[A-Za-z]{5,15}$/;
-        // if(!username.test(formData.username)) {
-        //   alert('Username harus terdiri dari 5-15 karakter');
-        //   return;
-        // }
-      
-        // const password = /^(?=.*[A-Z])(?=.*[0-9]).{6,10}$/
-        // if(!password.test(formData.password)) {
-        //   alert('Password harus terdiri dari 6-10 karakter dengan minimal satu huruf kapital dan satu angka');
-        //   return;
-        // }
-
-        // const email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        // if(!email.test(formData.email)) {
-        //   alert('Email harus memiliki format yang benar (contoh: example@example.com');
-        //   return;
-        // }
 
     // const SignUpPage = () => {
     //   const{formData, handleChange, handleSubmit} = useForm();
@@ -167,7 +167,7 @@ const SignUpPage = () => {
 
                         {/* Form */}
                         {/* <p ref={errReference} className={errMsg ? "errmsg" :  "offscreen"}>{errMsg}</p> */}
-                        <p ref={errReference} className={` ${errMsg ? "errmsg" : "offscreen"}`}>{errMsg}</p>
+                        <p ref={errReference} className={` ${errMsg ? "errmsg" : "offscreen"}`} aria-live="assertive">{errMsg}</p>
                         <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center p-14 pl-36 space-y-4'>
 
                             {/* Title */}
@@ -188,7 +188,7 @@ const SignUpPage = () => {
                                     onChange={(e) => setFullname(e.target.value)}
                                     required
                                     aria-invalid = {validFullname ? "false" : "true"}
-                                    aria-describedby="uidnote"
+                                    aria-describedby="note"
                                     onFocus={() => setFullnameFocus(true)}
                                     onBlur={() => setFullnameFocus(false)}
                                     />
@@ -204,7 +204,7 @@ const SignUpPage = () => {
                                     {/* </div> */}
 
                                     {/* Warning */}
-                                    <p id="uidnote" className={fullnameFocus && fullname && !validFullname ? "instructions" : "sr-only"}>
+                                    <p id="note" className={fullnameFocus && fullname && !validFullname ? "instructions" : "sr-only"}>
                                       <BiSolidInfoCircle/> 
                                       Must start with a capital letter
                                     </p>   
@@ -213,60 +213,134 @@ const SignUpPage = () => {
                                   {/* Username */}
                                   <div className="w-full bg-white flex items-center mb-[3%] border-gray-300 border rounded-lg px-3 py-2 focus:outline-none shadow shadow-black ">
                                       <MdPermIdentity className='m-[1%] text-slate-700'/>
-                                      <input
+                                      <input className="pl-2 py-1 w-72 focus:outline-none"
                                           type="text"
                                           name="username"
                                           placeholder="Username"
-                                          // value={formData.username}
-                                          // onChange={handleChange}
-                                          className="pl-2 py-1 w-72 focus:outline-none"
+                                          autoComplete="off"
+                                          onChange={(e) => setUname(e.target.value)}                       
                                           required
+                                          aria-invalid = {validUname? "false" : "true"}
+                                          aria-describedby="uname-note"
+                                          onFocus={() => setUnameFocus(true)}
+                                          onBlur={() => setUnameFocus(false)}
                                       />
+
+                                       {/* checkmark */}
+                                      {/* <div className="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center"  > */}
+                                      <span className={validUname ? "valid" : "hidden"}>
+                                          <FcCheckmark/>
+                                        </span>
+                                        <span className={`${validUname || !uname ? "hidden" : ""} text-red-600`}>
+                                          <FaTimes/>
+                                        </span>
+                                      {/* </div> */}
+
+                                      {/* Warning */}
+                                      <p id="uname-note" className={unameFocus && uname && !validUname? "instructions" : "sr-only"}>
+                                        <BiSolidInfoCircle/> 
+                                        Must be 5 to 15 characters <br/>
+                                        Letters, numbers are allowed
+                                      </p>   
                                   </div>
 
                                   {/* Email */}
                                   <div className=" bg-white flex items-center mb-[3%] border-gray-300 border rounded-lg px-3 py-2 focus:outline-none shadow shadow-black ">
                                       <FaRegEnvelope className='m-[1%] text-slate-700'/>
-                                      <input
+                                      <input className="pl-2 py-1 w-72 focus:outline-none "
                                           type="email"
                                           name="email"
-                                          placeholder="Email"
-                                          // value={formData.email}
-                                          // onChange={handleChange}
-                                          className="pl-2 py-1 w-72 focus:outline-none "
+                                          placeholder="Email"                                         
+                                          onChange={(e) => setEmail(e.target.value)}
                                           required
+                                          aria-invalid = {validEmail ? "false" : "true"}
+                                          aria-describedby="email-note"
+                                          onFocus={() => setEmailFocus(true)}
+                                          onBlur={() => setEmailFocus(false)}
                                       />
+
+                                        {/* checkmark */}
+                                        {/* <div className="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center"  > */}
+                                        <span className={validEmail ? "valid" : "hidden"}>
+                                            <FcCheckmark/>
+                                          </span>
+                                          <span className={`${validEmail || !email ? "hidden" : ""} text-red-600`}>
+                                            <FaTimes/>
+                                          </span>
+                                        {/* </div> */}
+
+                                        {/* Warning */}
+                                        <p id="email-note" className={emailFocus && email && !validEmail? "instructions" : "sr-only"}>
+                                          <BiSolidInfoCircle/> 
+                                          Letters, numbers, underscore and dot are allowed
+                                        </p>   
+
                                   </div>
 
                                   {/*Password*/}
                                   <div className="w-full bg-white flex items-center mb-[3%] border-gray-300 border rounded-lg px-3 py-2 focus:outline-none shadow shadow-black ">
                                       <MdLockOutline className='m-[1%] text-slate-700'/>
-                                      <input
+                                      <input className="pl-2 py-1 w-72 focus:outline-none"
                                           type="password"
                                           name="password"
                                           placeholder="Password"
-                                          // value={formData.password}
-                                          // onChange={handleChange}
-                                          className="pl-2 py-1 w-72 focus:outline-none"
+                                          onChange={(e) => setPass(e.target.value)}
                                           required
+                                          aria-invalid = {validPass ? "false" : "true"}
+                                          aria-describedby="pass-note"
+                                          onFocus={() => setPassFocus(true)}
+                                          onBlur={() => setPassFocus(false)}
                                       />
                                        <BiSolidHide/>
+
+                                       {/* checkmark */}
+                                        {/* <div className="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center"  > */}
+                                        <span className={validPass ? "valid" : "hidden"}>
+                                            <FcCheckmark/>
+                                          </span>
+                                          <span className={`${validPass || !pass ? "hidden" : ""} text-red-600`}>
+                                            <FaTimes/>
+                                          </span>
+                                        {/* </div> */}
+
+                                        {/* Warning */}
+                                        <p id="email-note" className={passFocus && pass && !validPass? "instructions" : "sr-only"}>
+                                          <BiSolidInfoCircle/> 
+                                          6 to 10 characters <br/>
+                                          Must have atleast 1 characters, 1 numbers
+                                        </p>                     
                                   </div>
                                   
                                   {/* Button Submit */}
+                                  {/* disabled={!validFullname || !validUname || !validEmail || !validPass  ? true : false}  */}
                                   <div className="text-center mb-6">
-                                    <button type="submit" className="bg-blue-500 text-white w-80 px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors duration-200">Register</button>
+                                    <button type="submit" className="bg-blue-500 text-white w-80 px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors duration-200">Sign Up</button>
                                   </div>
 
                                   {/* Sign in */}
                                   <div className="flex flex-col items-center mt-3">
                                     <p>Already a member?
-                                      <a href='/signInPage' className="text-blue-700 font-bold text-base leading-snug"> Sign In</a>
+                                      <a href='/signInPage' className="text-blue-700 font-bold text-base leading-snug">Sign In</a>
                                     </p>                  
-                                  </div>
-                                  
+                                  </div>                                  
                             </div>
                         </form>
+
+                        {/* pop up delete */}
+                        {success && (
+                        <div className='fixed inset-0 flex items-center justify-center z-50 bg-black opacity-75'>
+                          <div className='bg-slate-700 p-8 rounded-lg flex flex-col text-white'>
+                            <h2 className='text-xl font-semibold mb-4 text-white text-center'>Successfully Sign Up</h2>
+                            <FcCheckmark className="text-6xl text-emerald-500 animate-pulse mx-auto mb-4"/>
+                            <button
+                              onClick={() => setSuccess(false)} 
+                              className='bg-emerald-600 px-4 py-2 hover:bg-emerald-400 font-bold rounded-lg mt-4 text-white'
+                            >
+                              Close
+                            </button> 
+                          </div>
+                        </div>
+                        )}
                  </div>
             </div>
         </div>
